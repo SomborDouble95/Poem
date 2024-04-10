23996ff3bdad62ebe0fc1e94b724f88ca2af5486
SomborDouble95 <163771651+SomborDouble95@users.noreply.github.com>
2024-04-10 12:01:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let poemEls = document.querySelectorAll('.poem > p');
+
+
+
+// On Load of page
+window.addEventListener('load', () => {
+    gsap.from('.poem > p', {
+        duration: 20,
+        y: 100,
+        opacity: 0,
+        stagger: 2,
+        ease: 'power2.out'
+    });
+});
